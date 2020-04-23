65ace1015df85000294b102083bba797b5919b1b
Dupinder <dupinder.dhiman@catalystone.com>
2020-04-24 02:59:02 +0530

    service discovery initial commit

---
+++ src/test/java/io/doc/doscoveryserver/DoscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.doc.doscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
